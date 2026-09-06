6f48c1003cef80e81c8285db2b95ca9c1e95e4ee
ajaysinghdev14-source <ajaysingh.dev14@gmail.com>
2026-09-06 18:31:38 +0530

    add initial TypeScript implementation with basic functions

---
+++ 02-typescript/my-script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("Hello World!");
+
+function add(a, b) {
+  return `${a} : ${b}`;
+}
+const result = add(4, 5);
+console.log(result);
---
+++ 02-typescript/script.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(1, 2));
+console.log(add("mike", 2));
+// JS Design-Loosely Typed | Interpreted Language
+
+function printUser(userObj) {
+  console.log(userObj.fname);
+  console.log(userObj.lname);
+}
