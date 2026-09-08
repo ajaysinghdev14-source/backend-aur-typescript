d48c3946f44e3d4bdb6a0f48710f8ffc641602df
ajaysinghdev14-source <ajaysingh.dev14@gmail.com>
2026-09-08 19:24:23 +0530

    initialize server with Express framework; add basic route handler to return a JSON response

--- 04-express-typescript/src/app/index.ts
+++ 04-express-typescript/src/app/index.ts
@@ -2,5 +2,11 @@ import type { Application } from "express";
 import express from "express";
 
 export function createServerApplication(): Application {
-  return express();
+  const app = express();
+
+  app.get("/", function (req, res) {
+    return res.json({ message: "Hello World!" });
+  });
+
+  return app;
 }
--- 04-express-typescript/src/index.ts
+++ 04-express-typescript/src/index.ts
@@ -1,9 +1,10 @@
 import http from "node:http";
 import { env } from "./env.js";
+import { createServerApplication } from "./app/index.js";
 
 async function main() {
   try {
-    const server = http.createServer();
+    const server = http.createServer(createServerApplication());
     const PORT: number = env.PORT ? +env.PORT : 8080;
 
     server.listen(PORT, () => {
