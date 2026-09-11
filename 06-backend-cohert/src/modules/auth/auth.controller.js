aa7231e71f910681446c07285315c5246a4aa978
ajaysinghdev14-source <ajaysingh.dev14@gmail.com>
2026-09-11 10:53:24 +0530

    feat: implement user registration controller and service

--- 06-backend-cohert/src/modules/auth/auth.controller.js
+++ 06-backend-cohert/src/modules/auth/auth.controller.js
@@ -1,5 +1,11 @@
+import ApiResponse from "../../common/utils/api-response.js";
+import * as authService from "./auth.service.js";
+
 const register = async (req, res) => {
   // something
+  const userData = req.body;
+  const newUser = await authService.register(userData);
+  ApiResponse.success(res, newUser, "User registered successfully");
 };
 
 export { register };
---
+++ 06-backend-cohert/src/modules/auth/auth.service.js
@@ -0,0 +1,5 @@
+const register = async (userData) => {
+  // Implement the registration logic here, e.g., save the user to the database
+};
+
+export { register };
